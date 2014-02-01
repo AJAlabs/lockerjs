9692ebfb205bc15918e0442866e43a480e7f928c
AJ Acevedo <aj@ajalab.com>
2014-01-31 22:01:52 -0500

    Started working on the asset pipeline
    Added bootstrap Sass 3.1.0
    Added font-awesome 4.0.3
    
    Need to setup grunt task to concat and minify

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,19 +6,17 @@ module.exports = function(grunt) {
 
     // JSHint all JavaScript files
     jshint: {
-      files: ['Gruntfile.js', 'package.json', 'app.js', 'config/*'],
+      files: ['Gruntfile.js', 'package.json', 'server.js', 'app/**/*.js', 'config/**/*.js', 'config/**/*.json'],
     },
 
   });
 
   // Load the plug-ins
-  grunt.loadNpmTasks('grunt-notify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-manifest');
 
   // Default tasks
   grunt.registerTask('default', ['jshint']);
